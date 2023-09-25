370382a4e71ca0db2e57f4f165c0fea42a9474cf
guinnod <210103064@stu.sdu.edu.kz>
2023-09-25 14:05:42 +0600

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {},
+        colors: {
+            colors: {
+                primary: "#9969FF",
+                white: {
+                    default: "#FFFFFF",
+                    ghost: "#FBFBFB",
+                },
+                black: {
+                    default: "#000000",
+                    custom: "#180A29",
+                },
+                gray: {
+                    custom: "#F2F1F4",
+                    cloud: "#EDEBEF",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
